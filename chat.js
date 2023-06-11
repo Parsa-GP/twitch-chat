f4787c5a87173ab3573221a9b758251d52a33799
ParsaGP <psrzp1386@gmail.com>
2023-06-11 14:56:24 +0330

    * Rewrite the chat display system + Adding more badge & fix old badges

--- chat.js
+++ chat.js
@@ -33,30 +33,14 @@ client.on('message', (channel, tags, message, self) => {
 
     const container = document.querySelector('#chat-container');
     const messageContainer = document.createElement('div');
-    const userInfo = document.createElement('span');
-    const newMessage = document.createElement('span');
 
     messageContainer.classList.add('message');
-
-    if(tags['mod'] === true) {
-        userInfo.innerHTML = `<img src="img/mod.png" class="badge mod"> <span class="display-name" style="color:${tags['color']};">${tags['display-name']}:</span> `;
-        userInfo.classList.add('mod');
-    } else if(tags['vip'] === true) {
-        userInfo.innerHTML = `<img src="img/vip.png" class="badge vip"> <span class="display-name" style="color:${tags['color']};">${tags['display-name']}:</span> `;
-        userInfo.classList.add('vip');
-    } else if(tags['subscriber'] === true) {
-        userInfo.innerHTML = `<img src="img/sub.png" class="badge sub"> <span class="display-name" style="color:${tags['color']};">${tags['display-name']}:</span> `;
-        userInfo.classList.add('sub');
-    } else {
-        userInfo.innerHTML = `<span style="color:${tags['color']};">${tags['display-name']}</span>: `;
-        userInfo.classList.add('usr');
+    let badges = "";
+    for (const k in tags["badges"]) {
+        badges += `<img src="img/badges/${k}.png" onerror="this.onerror=null;this.style.display = 'none'" class="badge ${k}">`
     }
+    messageContainer.innerHTML = `${badges} <span style="color:${tags['color']};" class="display-name">${tags['display-name']}: </span>${messageWithEmoticons}`;
 
-    newMessage.innerHTML = messageWithEmoticons;
-    newMessage.classList.add('text-indigo-800', 'flex', 'gap-1', 'flex-wrap', 'mt-2');
-
-    messageContainer.appendChild(userInfo);
-    messageContainer.appendChild(newMessage);
     container.appendChild(messageContainer);
 
     const messages = document.querySelectorAll('.message');
